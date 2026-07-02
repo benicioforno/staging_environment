060a24f55917797655fc318c1a7cb61ffca17893
Benício Reinehr Dal Forno <beniciordf@gmail.com>
2026-07-02 14:02:11 -0300

    Create endpoint to deploy tomcat

---
+++ src/main/java/com/benicioforno/myapp/controller/HomeController.java
@@ -0,0 +1,17 @@
+package com.benicioforno.myapp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/myapp")
+public class HomeController {
+
+    @GetMapping("/")
+    public String home(){
+        return """
+                <h1>Deploy 1</h1>
+                <h2>Benício Reinehr Dal Forno</h2>
+                <h2>02/07/2026</h2>
+                """;
+    }
+}
